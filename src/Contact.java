2361259f39a53569b486aa90ed472c761117c69f
AkobirToshtemirov <toshtemirovakobir8@gmail.com>
2023-04-25 09:33:24 +0500

    initial  commit

--- src/Contact.java
+++ src/Contact.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class Contact {
+import java.util.List;
+
+public class Contact {
+    private String name;
+    private int phone;
+    private String email;
+
+    public Contact() {
+    }
+
+    public Contact(String name, int phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void viewAllContact(List<Contact> contacts) {
+        int i = 1;
+        for (Contact contact : contacts) {
+            System.out.println("#" + i + " Contact information: ");
+            System.out.println("Name: " + contact.name);
+            System.out.println("Phone #: " + contact.phone);
+            System.out.println("Email: " + contact.email);
+            System.out.println();
+            i++;
+        }
+    }
+
+    public void addContact() {
+    }
+
+    public void editContact() {
+    }
+
+    public void deleteContsct() {
+    }
+
+    public void searchContact() {
+    }
+
+    public void sortContact() {
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Contact contact = new Contact();
+        List<Contact> contacts = new ArrayList<>();
+
+        contacts.add(new Contact("John Doe", 7717085, "johnbdoe@gmail.com"));
+        contacts.add(new Contact("Joe Black", 888934983, "joeblack@gmail.com"));
+        contacts.add(new Contact("Susan Black", 34234324, "susanblack@gmail.com"));
+
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Welcome to Address Book App!");
+            System.out.println();
+
+            while (true) {
+                System.out.println("""
+                        MENU:
+                        1. VIEW_ALL_CONTACT
+                        2. ADD_CONTACT
+                        3. EDIT_CONTACT
+                        4. DELETE_CONTACT
+                        5. SEARCH_CONTACT
+                        6. SORT_CONTACT
+                        7. EXIT
+                        """);
+
+                System.out.print("Option: ");
+                String option = sc.nextLine();
+
+                switch (option) {
+                    case "1" -> contact.viewAllContact(contacts);
+                    case "2" -> contact.addContact();
+                    case "3" -> contact.editContact();
+                    case "4" -> contact.deleteContsct();
+                    case "5" -> contact.searchContact();
+                    case "6" -> contact.sortContact();
+                    case "7" -> System.exit(0);
+                    default -> System.out.println("Invalid value is added. Please try again!");
+                }
+            }
+        }
+
     }
 }
